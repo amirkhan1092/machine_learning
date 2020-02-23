2ffc70af875e7280e367308d19cf5028bd3f5edd
Amir khan <amirkhan1092@gmail.com>
2020-02-24 00:07:28 +0530

    model intercept and slope

--- regression_linear.py
+++ regression_linear.py
@@ -8,3 +8,8 @@ y = np.array([5, 20, 14, 32, 22, 38])
 model = LinearRegression().fit(x, y)
 r_sq = model.score(x, y)
 print('coefficient of determination:', r_sq)
+
+print('intercept:', model.intercept_)
+# intercept: 5.633333333333329
+print('slope:', model.coef_)
+# slope: [0.54]
