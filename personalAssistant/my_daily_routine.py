7901b6c6c611e5012b2200109eb0384a2cf6592e
amirkhan1092 <amirkhan1092@gmail.com>
2020-04-26 17:58:15 +0530

    personal time routine

--- personalAssistant/my_daily_routine.py
+++ personalAssistant/my_daily_routine.py
@@ -1,10 +1,18 @@
 # @auther amirkhan1092
 import matplotlib.pyplot as plt
 
-hours = [2, 6, 3, 2, 11]
-activity = ['Eat', 'Sleep', 'Ibadat', 'Social Media', 'coding']
+hours = [2, 6, 4, 2, 10]
+activity = ['Eat', 'Sleep', 'Ibadat',
+            'Social Media', 'Programming']
 
 color = ['r', 'k', 'g', 'c', 'b']
 plt.pie(hours, labels=activity, colors=color, startangle=90,
         autopct='%.1f%%')
 plt.show()
+
+
+
+
+
+
+
