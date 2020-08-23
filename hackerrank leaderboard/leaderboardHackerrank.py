52ccf323d32988bbe4a7600663cf5cf91cc6118d
amirkhan1092 <amirkhan1092@gmail.com>
2020-08-23 12:36:34 +0530

    csv files

---
+++ hackerrank leaderboard/leaderboardHackerrank.py
@@ -0,0 +1,75 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+time.sleep(3)
+
+##contest = 'b22-csea'  # add your contest name
+'''b22-csea
+b22-cseb
+b22-csec
+b22-csed
+b22-csee
+b22-csef
+b22-cseg
+b22-cseh
+b22-csei
+b22-csej
+b22-csek
+b22-csel
+b22-csem
+b22-cseop
+b22-ecea
+b22-eceb
+b22-mca'''
+
+
+lst = '''b22-csea
+b22-cseb
+b22-csec
+b22-csed
+b22-csee
+b22-csef
+b22-cseg
+b22-cseh
+b22-csei
+b22-csej
+b22-csek
+b22-csel
+b22-csem
+b22-cseop
+b22-ecea
+b22-eceb
+b22-mca'''.split()
+
+
+for contest in lst:
+    time.sleep(5)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    link = f"https://www.hackerrank.com/contests/{contest}/leaderboard"
+    driver.get(link)
+    
+    ##driver.find_element_by_class_name('icon-cancel-small').click()
+    page_source = driver.page_source
+    try:
+        el = driver.find_element_by_id('pagination-length')
+        for option in el.find_elements_by_tag_name('option'):
+            if option.text == '100':
+                option.click()
+                time.sleep(8)
+
+        leaderboardPage = driver.page_source
+
+    except:
+        print('contest empty')
+    else:
+        soup = BeautifulSoup(leaderboardPage, 'html.parser')
+        time.sleep(5)
+        a_tags = soup.findAll("a", class_="cursor leaderboard-hackername rg_5")
+        for a_tag in a_tags:
+            with open(f'csv/all-{time.asctime()[:11]}.csv', 'a') as f:
+                print(a_tag['data-value'], a_tag['data-attr10'], sep=',', file=f)
+
+    driver.quit()
+
