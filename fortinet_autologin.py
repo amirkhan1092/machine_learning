89bd223734efc04c3d6c2ff31c0804f045ea88a5
amirkhan1092 <amirkhan1092@gmail.com>
2020-08-28 10:43:30 +0530

    files

--- fortinet_autologin.py
+++ fortinet_autologin.py
@@ -2,19 +2,39 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
-
+import keyboard
+import time
 # driver = webdriver.Chrome()
 
-driver.get('172.16.10.20:1000/login?')
+driver.get('http://glauniversity.in:8088/EduwareHome/Home#')
 
-user_name = driver.find_element_by_xpath('//*[@id="ft_un"]')
-user_name.send_keys('amir.khan')
+user_name = driver.find_element_by_id('txt_user_name')
+user_name.send_keys('GLA118309')
+password = driver.find_element_by_id('txt_password')
+password.click()
+time.sleep(3)
+keyboard.write('amir@gla')
+keyboard.press('enter')
 
-password = driver.find_element_by_xpath('//*[@id="ft_pd"]')
-password.send_keys('54892044')
+# password = driver.find_element_by_id('dl_role')
+# password.send_keys('amir@gla')
 
-button = driver.find_element_by_xpath('/html/body/div/div/form/div[3]/input')
+# psd = driver.find_element_by_class_name('row login-form')
+# psd.send_keys('amir@gla')
+
+
+
+# password = driver.find_element_by_id('txt_password')
+# password.send_keys('amir@gla')
+
+button = driver.find_element_by_id('btn_login')
 button.click()
+
+# password = driver.find_element_by_xpath('//*[@id="ft_pd"]')
+# password.send_keys('54892044')
+#
+# button = driver.find_element_by_xpath('/html/body/div/div/form/div[3]/input')
+# button.click()
 
 
 
