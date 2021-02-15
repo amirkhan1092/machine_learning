02323ec132511140d46719a07b73601948bef15c
amirkhan1092 <amirkhan1092@gmail.com>
2021-02-16 00:48:14 +0530

    camera setting to show the screen items

--- Smart CCTV/main.py
+++ Smart CCTV/main.py
@@ -6,7 +6,7 @@ while cam.isOpened():
     ret, image = cam.read()
     cv2.imshow('Khan Camera', image)
 
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(10) == ord('q'):  # wait for 10ms for wait-key
         break
 
 cv2.destroyAllWindows()
