2c685a93daa61511b634159fe2d08893592d3d5c
amirkhan1092 <amirkhan1092@gmail.com>
2021-02-16 01:04:28 +0530

    contour mapped

--- Smart CCTV/main.py
+++ Smart CCTV/main.py
@@ -8,8 +8,12 @@ while cam.isOpened():
 
     diff = cv2.absdiff(image1, image2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('Khan Camera', gray)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    dilated = cv2.dilate(thresh, None, iterations=3)
+    contour, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(image1, contour, -1, (0, 255, 0), 2)
+    cv2.imshow('Khan Camera', image1)
     if cv2.waitKey(10) == ord('q'):  # wait for 10ms for wait-key
         break
 
