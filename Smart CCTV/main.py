afa457331558a2416ea0ca2ebc3b6d2afef24347
amirkhan1092 <amirkhan1092@gmail.com>
2021-02-16 00:55:28 +0530

    image absolute difference with gray color

--- Smart CCTV/main.py
+++ Smart CCTV/main.py
@@ -3,9 +3,13 @@ import cv2
 cam = cv2.VideoCapture(0)
 
 while cam.isOpened():
-    ret, image = cam.read()
-    cv2.imshow('Khan Camera', image)
+    ret, image1 = cam.read()
+    ret, image2 = cam.read()
 
+    diff = cv2.absdiff(image1, image2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Khan Camera', gray)
     if cv2.waitKey(10) == ord('q'):  # wait for 10ms for wait-key
         break
 
